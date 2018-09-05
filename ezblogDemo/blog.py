b749168e5a3b7d8fd8d552dfb83253078b7b06a4
lit <t>
2018-09-05 19:55:24 +0800

    20180905

--- ezblogDemo/blog.py
+++ ezblogDemo/blog.py
@@ -1,10 +1,19 @@
-
 import os
 import time
-from flask import Flask,render_template,redirect,abort,request,session,url_for
+from flask import Flask, render_template, redirect, abort, request, session,url_for
+from webargs import fields, validate
+from flask_sqlalchemy import SQLAlchemy
 
 conments = []
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:////tmp/test.db'    #加上数据库配置
+db = SQLAlchemy(app)    #创建SQLALchemy对象，将flask app作为参数传入
+
+"""
+SQLALchemy对象创建后，会包含sqlalchemy及sqlalchemy.orm中的所有方法及属性，
+其中Model类用于声明模型时到declarative基类
+
+"""
 
 
 @app.route('/testPost/',methods=['POST','GET'])
@@ -34,8 +43,34 @@ def index():
                     'createTime': date
                     })
 
-        print(conments)
         return redirect(url_for('index'))
+
+"""
+数据类型：
+Integer-自增长类型
+
+
+"""
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    created_time = db.Column()
+    title = db.Column()
+    conment = db.Column()
+
+    def __init__(self, username, title, conment):
+        self.username = username
+        self.created_time = []
+
+        self.title = title
+        self.conment = conment
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1',port=8080,debug=True)
